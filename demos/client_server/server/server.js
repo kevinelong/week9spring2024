1e1286e490b37fe1ecbebd9dbc5fd7d0a6cb69c7
kevinelong <kevinelong@gmail.com>
2024-06-04 13:01:26 -0700

    WIP

---
+++ demos/client_server/server/server.js
@@ -0,0 +1,118 @@
+let express = require("express");
+let fs = require("fs");
+let cors = require("cors");
+
+let app = express();
+app.use(cors());
+
+// Create application/x-www-form-urlencoded parser
+let urlencodedParser = express.urlencoded({ extended: false });
+app.use(express.json());
+
+////////////////////////////////////////////////////////////
+// API endpoints
+
+// GET all courses
+app.get("/api/courses", function (req, res) {
+    console.log("LOG: Got a GET request for all courses");
+
+    let data = fs.readFileSync(__dirname + "/data/" + "courses.json", "utf8");
+    data = JSON.parse(data);
+
+    // LOG returned data
+    console.log("LOG: Returned courses -> ");
+    console.log(data);
+    data.sort((a,b)=> {
+        if(a.courseName < b.courseName){
+            return -1
+        }
+        if(a.courseName > b.courseName){
+            return 1
+        }
+        return 0 //equal
+    });
+
+    res.end(JSON.stringify(data));
+});
+
+/////////////////////////////////////////////////////
+// Start the server
+
+app.use(express.static("public"));
+
+let server = app.listen(8081, function () {
+    let port = server.address().port;
+    console.log("LOG: App listening at port %s", port);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// GET one course by id
+// app.get("/api/courses/:id", function (req, res) {
+//     let id = req.params.id;
+//     console.log("LOG: Got a GET request for course " + id);
+
+//     let data = fs.readFileSync(__dirname + "/data/" + "courses.json", "utf8");
+//     data = JSON.parse(data);
+
+//     // Find the course
+//     let match = data.find(c => c.id == id);
+
+//     // If course not found
+//     if (match == undefined) {
+//         console.log("LOG: **NOT FOUND**: course " + id + " does not exist!");
+//         res.status(404).send();   // not found
+//         return;
+//     }
+
+//     // LOG returned data
+//     console.log("LOG: Returned course -> ");
+//     console.log(match);
+
+//     res.end(JSON.stringify(match));
+// });
+
+
+
+
+/*
+app.post("/api/courses", urlencodedParser, function (req, res) {
+    console.log("LOG: Got a POST request for courses");
+    console.log("LOG: Mesage body --------> ");
+    console.log(JSON.stringify(req.body));
+    res.status(201).json(course);
+ })
+
+ app.put("/api/courses/:id", function (req, res) {
+    console.log("LOG: Got a PUT request for a course");
+    res.status(200).send();
+ })
+ 
+ app.delete("/api/courses/:id", function (req, res) {
+    console.log("LOG: Got a DELETE request for a course");
+    res.status(200).send();
+ })
+*/
+
+
+
